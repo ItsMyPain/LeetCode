99b98267b0967c95f3e0b2bf4d64a4e21281a1c4
Sergey <agibalov1294@gmail.com>
2023-04-03 10:55:16 +0300

    added 329

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -1,2 +1,16 @@
 def test():
-    pass
+    s = "axc"
+    t = "ahbgdc"
+    i = 0
+    j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    if i == len(s):
+        return True
+    else:
+        return False
+
+
+print(test())
